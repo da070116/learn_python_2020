9b15be1828c6f3a89f827abc9bcdff7cb118b564
Alexander Dubrovin <da070116@gmail.com>
2020-09-06 10:10:15 +0300

    Простой echo-бот

--- bot/stlgrmbot.py
+++ bot/stlgrmbot.py
@@ -1,20 +1,28 @@
 import logging
-from telegram.ext import CommandHandler, Updater
-
-logging.basicConfig(filename='tlgrm.log', level=logging.INFO)
+from telegram.ext import CommandHandler, Updater, MessageHandler, Filters
+from . import settings
 
 
 def greet(update, context):
     update.message.reply_text(f"Hello, {update.effective_user.first_name}! I'm started")
 
 
+def echo(update, context):
+    received_message = update.message.text
+    update.message.reply_text(received_message)
+
+
 def main():
-    tlgrmbot = Updater("1183917859:AAHU7oW4BB1GJBIICeHl6Hqbcq22m0LVzsw", use_context=True)
+    tlgrmbot = Updater(settings.API_KEY, use_context=True)
     dispatcher = tlgrmbot.dispatcher
     dispatcher.add_handler(CommandHandler('start', greet))
+    dispatcher.add_handler(MessageHandler(Filters.text, echo))
+    logging.info("Bot started")
     tlgrmbot.start_polling()
     tlgrmbot.idle()
 
 
 if __name__ == '__main__':
+    formatter = '%(levelname)s at %(asctime)s:%(name)s:%(message)s'
+    logging.basicConfig(filename='tlgrm.log', level=logging.INFO, format=formatter)
     main()
