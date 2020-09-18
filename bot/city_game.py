320efea37cfb2c052d86bcfe280c59016d57c55c
Alexander Dubrovin <da070116@gmail.com>
2020-09-18 15:33:53 +0300

    Рефакторинг функции для игры в города

---
+++ bot/city_game.py
@@ -0,0 +1,79 @@
+import re
+from random import randint
+from typing import List
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+
+def get_cities_from_site() -> List[str]:
+    soup = BeautifulSoup(
+        urlopen('http://www.1000mest.ru/cityA'), 'html.parser'
+    )
+    return [a.get('href')
+            for a in soup.find_all('a') if a.get('href').startswith('city')
+            ]
+
+
+def obtain_cities_list():
+    print("OSL started")
+    cities_list = []
+    tag_rem = re.compile(r'<[^>]+>')
+    for link in get_cities_from_site():
+        soup = BeautifulSoup(
+            urlopen(f'http://www.1000mest.ru/{link}'), 'html.parser'
+        )
+        for x in soup.find_all('td'):
+            city_name = tag_rem.sub('', str(x))
+            if '(' in city_name:
+                city_name = city_name.split('(')[0]
+            cities_list.append(city_name)
+    print(len(cities_list))
+    return cities_list
+
+
+def get_last(city_val, city_list):
+    for letter in city_val[::-1]:
+        for item in city_list:
+            if item.lower()[0] == letter:
+                return letter
+
+
+def initialize(data):
+    _cities = None
+    _first_letter = None
+    _user_city = None
+    if len(data.args) > 0:
+        _user_city = data.args[0].strip()
+    if not data.user_data.get("progress") or data.user_data.get("end_game"):
+        data.user_data['end_game'] = False
+        _cities = obtain_cities_list()
+        _first_letter = _user_city.lower()[0] if _user_city else None
+    else:
+        _cities = data.user_data['cities']
+        _first_letter = data.user_data['first_letter']
+    data.user_data["progress"] = True
+    return _user_city, _first_letter, _cities
+
+
+def city(update, context):
+    (user_city, first_letter, cities) = initialize(context)
+    if None in (user_city, first_letter, cities):
+        result = "Critical error"
+        context.user_data['end_game'] = True
+        context.user_data["progress"] = False
+    elif not user_city.lower()[0] == first_letter:
+        result = f"First letter of your city should be {first_letter}"
+    else:
+        cities.remove(user_city)
+        ll = get_last(user_city, cities)
+        answer_list = [a for a in cities if a.lower().startswith(ll)]
+        if len(answer_list) == 0:
+            result = "No city left. You win"
+            context.user_data['end_game'] = True
+        else:
+            answer = answer_list[randint(0, len(answer_list)-1)]
+            result = f"{user_city} - {answer}"
+            cities.remove(answer)
+            context.user_data["first_letter"] = get_last(answer, cities)
+            context.user_data["cities"] = cities
+    update.message.reply_text(result)
--- bot/stlgrmbot.py
+++ bot/stlgrmbot.py
@@ -1,94 +1,9 @@
 import logging
-import re
-# from random import randint
-from random import randint
-from typing import List
-from urllib.request import urlopen
 from datetime import datetime
 import ephem
 from telegram.ext import CommandHandler, Updater, MessageHandler, Filters
 from bot import settings
-from bs4 import BeautifulSoup
-
-
-def get_links() -> List[str]:
-    soup = BeautifulSoup(
-        urlopen('http://www.1000mest.ru/cityA'), 'html.parser'
-    )
-    return [a.get('href')
-            for a in soup.find_all('a') if a.get('href').startswith('city')
-            ]
-
-
-def obtain_cities_list():
-    get_links()
-    cities_list = []
-    tag_rem = re.compile(r'<[^>]+>')
-    for link in get_links():
-        soup = BeautifulSoup(
-            urlopen(f'http://www.1000mest.ru/{link}'), 'html.parser'
-        )
-        for x in soup.find_all('td'):
-            city_name = tag_rem.sub('', str(x))
-            if '(' in city_name:
-                city_name = city_name.split('(')[0]
-            cities_list.append(city_name)
-    return cities_list
-
-
-def get_last_letter(city_val, city_list):
-    for letter in city_val[::-1]:
-        for item in city_list:
-            if item.lower()[0] == letter:
-                return letter
-
-
-def city(update, context):
-    answer_city = None
-    user_input = context.args[0].lower().strip()
-    if context.user_data.get('game') is None \
-            or context.user_data.get('end_game') is True:
-        context.user_data['game'] = True
-        cities = obtain_cities_list()
-        first_letter = user_input[0]
-        # print(f'First letter from input {first_letter=}')
-    else:
-        cities = context.user_data['cities']
-        first_letter = context.user_data['first_letter']
-        # print(f'First letter from context {first_letter=}')
-    if user_input in [city_name.lower() for city_name in cities]:
-        if first_letter == user_input[0]:
-            last_letter = get_last_letter(user_input, cities)
-            answer_list = [a.lower() for a in cities
-                           if a.lower()[0] == last_letter
-                           and not a.lower() == user_input
-                           ]
-            # print(answer_list)
-            if len(answer_list) > 0:
-                max_value = len(answer_list) - 1
-                city_id = randint(0, max_value)
-                answer_city = answer_list[city_id]
-                # print(answer_city)
-                # print(f"{cities=}, {len(cities)=}")
-                update.message.reply_text(
-                    f'Your city was {user_input.capitalize()},'
-                    f' my answer is {answer_city.capitalize()}'
-                )
-            else:
-                update.message.reply_text("No city left to reply")
-                context.user_data['end_game'] = True
-            last_letter = get_last_letter(answer_city, cities)
-            context.user_data['cities'] = cities
-            context.user_data['first_letter'] = last_letter
-        else:
-            update.message.reply_text(
-                f'Your city should start with {first_letter}'
-            )
-    else:
-        context.user_data['cities'] = cities
-        update.message.reply_text(
-            f'Your city ({user_input.capitalize()}) is not in known list'
-        )
+from bot.city_game import city
 
 
 def greet(update, context):
